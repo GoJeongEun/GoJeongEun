d4c44ca2efa360ad06391974ee9f48e0b66f1dbd
GoJeongEun <kone31964@gmail.com>
2024-08-27 17:33:08 +0900

    24.08.24 수정본

--- js/sub.js
+++ js/sub.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
      
 
     $("#datepickerAfter, #datepickerBefroe").datepicker({
-        dateFormat: 'yy-mm-dd' //Input Display Format 변경
+        dateFormat: 'yyyy-mm-dd' //Input Display Format 변경
         ,showOtherMonths: true //빈 공간에 현재월의 앞뒤월의 날짜를 표시
         ,showMonthAfterYear:true //년도 먼저 나오고, 뒤에 월 표시
         ,changeYear: true //콤보박스에서 년 선택 가능
