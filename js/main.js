ca8aafcf31b6932522247ebb22b101586c5e32a4
GoJeongEun <kone31964@gmail.com>
2024-09-13 13:19:27 +0900

    9/13일 수정

--- js/main.js
+++ js/main.js
@@ -12,8 +12,11 @@ $(document).ready(function(){
        }
     });
     
-    $('#slider').slick({
-
+    //tab 전환 예시
+    $(".class명").click(function(){
+     // class명 children에 해당하는 태그 넣어주기
+     $(this).children('button').removeClass('active');
+     $(this).children('button').addClass('active');
     });
 });
 
