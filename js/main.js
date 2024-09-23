7be5649e02ca531e164bd237f9671289ee532085
GoJeongEun <kone31964@gmail.com>
2024-09-23 15:03:29 +0900

    24.09.23 업데이트

--- js/main.js
+++ js/main.js
@@ -20,4 +20,32 @@ $(document).ready(function(){
     });
 });
 
+ // a태그 id href 클릭시 smooth scrolling
+ // css 속성 scroll-behavior: smooth; 이 적용되지 않을 경우 아래의 script를 사용하여 같은 효과를 준다(크로스브라우저)
+
+$(document).ready(function(){
+     // Add smooth scrolling to all links
+     $("a").on('click', function(event) {
+   
+       // Make sure this.hash has a value before overriding default behavior
+       if (this.hash !== "") {
+         // Prevent default anchor click behavior
+         event.preventDefault();
+   
+         // Store hash
+         var hash = this.hash;
+   
+         // Using jQuery's animate() method to add smooth page scroll
+         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+         $('html, body').animate({
+           scrollTop: $(hash).offset().top
+         }, 800, function(){
+      
+           // Add hash (#) to URL when done scrolling (default click behavior)
+           window.location.hash = hash;
+         });
+       } // End if
+     });
+});
+
 
