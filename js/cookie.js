32374da0dc7a255174f7041454ef58fc196f616e
GoJeongEun <kone31964@gmail.com>
2024-10-10 17:57:03 +0900

    쿠키 팝업 테스트

--- js/cookie.js
+++ js/cookie.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+$(function(){
 
-   // 쿠키 설정
+   // 쿠키 값 확인 show, hide 처리
      cookiedata = document.cookie;
     if (cookiedata.indexOf("maindiv=done") < 0){
         querySelectorAll["layer_popup"].style.visibility = "visible"; 
@@ -10,10 +10,16 @@ $(document).ready(function(){
     }
 });
 
+// 쿠키 데이터 설정 
 function setCookie(name, value, expiredays) {
     var todayDate = new Date();
     todayDate.setDate( todayDate.getDate() + expiredays);
     document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";"
-
 }
 
+function closePop(){
+    if(document.todayChk.checked){
+        setcookie("maindiv", "done" , 1);
+    }
+    querySelectorAll["layer_pop"].style.visibility = "hidden";
+}
